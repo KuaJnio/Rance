2a72fd50a13acbf510d4bf01b0038502de855d53
David JAFFRE <david.jaffre@orange.com>
2018-08-03 14:25:28 +0200

    scenes

---
+++ source/js/scenes.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".scene-1").hide();
+    $(".scene-2").hide();
+    $(".scene-3").fadeIn();
+    $(".goto-scene-1").click(function(){
+	    $(".scene-1").fadeIn();
+	    $(".scene-2").hide();
+	    $(".scene-3").hide();
+    });
+    $(".goto-scene-2").click(function(){
+	    $(".scene-1").hide();
+	    $(".scene-2").fadeIn();
+	    $(".scene-3").hide();
+    });
+    $(".goto-scene-3").click(function(){
+	    $(".scene-1").hide();
+	    $(".scene-2").hide();
+	    $(".scene-3").fadeIn();
+    });
+});
