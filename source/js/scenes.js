face7a241259247fb5b1974baed8e3cc2d629bbc
Romain DUPONT <romain.dupont@caramail.fr>
2018-08-13 20:03:33 +0200

    changed footer + add columns

--- source/js/scenes.js
+++ source/js/scenes.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $(".scene-1").hide();
+    $(".scene-1").show();
     $(".scene-2").hide();
-    $(".scene-3").show();
+    $(".scene-3").hide();
     $(".goto-scene-1").click(function(){
 	    $(".scene-1").fadeIn();
 	    $(".scene-2").hide();
